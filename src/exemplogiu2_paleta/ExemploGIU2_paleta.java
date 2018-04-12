0cd0787dc5b4d24e2c0e2871b4b4e8bf795c3043
acabezaslopez <acabezaslopez@victoria11.danielcastelao.org>
2018-04-12 13:31:00 +0200

    exemplo

---
+++ src/exemplogiu2_paleta/ExemploGIU2_paleta.java
@@ -0,0 +1,17 @@
+package exemplogiu2_paleta;
+
+/**
+ *
+ * @author acabezaslopez
+ */
+public class ExemploGIU2_paleta {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ventana v = new Ventana();
+        v.setVisible(true);
+    }
+
+}
